535eb804d91eb4f193bb23680e4165c51a2f69b2
Nathaly Uruchima <nuruchi1@binghamton.edu>
2019-10-14 00:09:36 -0400

    add code and edit readme file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
+For this project I created a basic brick breaker game! Brick breaker was a childhood favorite of mine!
+
+This app was created in Xcode and has one basic level. There is a score keeper which will increase each time the ball hits a brick. Since the score is rather large on the screen a nice touch is that the ball goes behind the score counter. There is a win and lose screen which I have shown in the screen shots.: 
 # p03_uruchima
--- project3-basicGame/project3-basicGame/GameScene.swift
+++ project3-basicGame/project3-basicGame/GameScene.swift
@@ -9,21 +9,89 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    var ball = ["whiteSphere"]
+    var ball:SKSpriteNode!
+    var paddle:SKSpriteNode!
+    var scoreLabel:SKLabelNode!
+    var score:Int = 0{
+        didSet{
+            scoreLabel.text = "Score:\(score)"
+        }
+    }
+        
+        
+        
+    
     
     override func didMove(to view: SKView) {
-        let background = SKSpriteNode(imageNamed: "whiteSphere")
-        background.position = CGPoint(x: frame.midX, y: frame.midY)
-        background.alpha = 0.2
-        background.zPosition = -1
+        ball = self.childNode(withName: "Ball") as! SKSpriteNode
+        paddle = self.childNode(withName: "Paddle") as! SKSpriteNode
+        scoreLabel = self.childNode(withName: "Score") as! SKLabelNode
+        ball.physicsBody?.applyImpulse(CGVector(dx: 50, dy: 50))
         
+        
+        
+        let border = SKPhysicsBody(edgeLoopFrom: (view.scene?.frame)!)  //create boarder
+        border.friction = 0;    //friction is zero so boarder doesn slow ball down
+        self.physicsBody = border
+        
+        
+        self.physicsWorld.contactDelegate = self  //allows us to use contact delegate within self
     }
     
-    
-    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            let touchLocation = touch.location(in:self)
+            paddle.position.x = touchLocation.x //we are moving (teleport)the paddle to whereever we touch in the x axis
+            
+        }
+    }
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+                   let touchLocation = touch.location(in:self)
+                   paddle.position.x = touchLocation.x //we are moving (animation)the paddle to whereever we touch in the x axis
+               }
+    }
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        //Winning logic
+        if (score == 20){
+            scoreLabel.text = "You Won!"
+            self.view?.isPaused = true;
+        }
+        
+        if(ball.position.y < paddle.position.y){
+            scoreLabel.text = "You Lost!"
+            self.view?.isPaused = true
+            
+            
+        }
     }
+
+    func didBegin(_ contact: SKPhysicsContact) {
+        /*
+         bodyAName and bodyBName are returning the contacts between objects
+         */
+        let bodyAName = contact.bodyA.node?.name
+        let bodyBName = contact.bodyB.node?.name
+        
+        if bodyAName == "Ball" && bodyBName == "Brick" || bodyAName == "Brick" && bodyBName == "Ball" {
+            if bodyAName == "Brick"{
+                
+                contact.bodyA.node?.removeFromParent()
+                score+=1
+            }
+            else if bodyBName == "Brick"{
+                contact.bodyB.node?.removeFromParent()
+                score+=1
+                
+            }
+            
+            
+        }
+    }
+    
+    
+
 }
